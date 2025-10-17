8c691d3362dc25f0a926220400585e25b9d3de55
Calum Rees <calumrees99@gmail.com>
2025-10-17 08:58:54 +0100

    removed default for budget ammount

--- modules/governance/consumption/variables.tf
+++ modules/governance/consumption/variables.tf
@@ -1,22 +1,21 @@
 variable "budgetName" {
   type        = string
-  description = "The name which should be used for this budget"
+  description = "Required - The name which should be used for this budget"
 }
 
 variable "budgetAmount" {
   type        = number
-  description = "The total amount of cost to track with the budget."
-  default     = 10
+  description = "Required - The total amount of cost to track with the budget."
 }
 
 variable "timeGrain" {
   type        = string
-  description = "The time covered by a budget. Tracking of the amount will be reset based on the time grain."
+  description = "Required - The time covered by a budget. Tracking of the amount will be reset based on the time grain."
 }
 
 variable "start_date" {
   type        = string
-  description = "The start date for the budget. The start date must be first of the month and should be less than the end date"
+  description = "Required - The start date for the budget. The start date must be first of the month and should be less than the end date"
   default     = "2025-10-01T00:00:00Z"
 }
 
